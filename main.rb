f361596fc74a72095f3ef481391fb8bab9557d56
MLigeon <mfligeon@gmail.com>
2018-02-18 19:15:48 +0100

    Added count_1 and count_org methodddd!

--- main.rb
+++ main.rb
@@ -17,8 +17,27 @@ def include_org
   org = read_contacts.select {|x| x[:"email"].include?("org")}
 end
 
+def count_1
+  include_1.reduce(0) do |sum, element|
+  sum = include_1.count
+ end
+end
+
+
+def count_org
+   include_org.reduce(0) do |sum, element|
+   sum = include_org.count
+  end
+end
+
+
+
 puts show_contact
 puts
 puts include_1
 puts
 puts include_org
+puts
+puts count_1
+puts
+puts count_org
